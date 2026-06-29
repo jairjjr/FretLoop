aa134c08275a98eb358ccf3591303e5557e9f71b
jairjjr <jairjjr@gmail.com>
2026-06-29 08:43:18 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Sequencer } from './components/Sequencer/Sequencer';
 import { Fretboard } from './components/Fretboard/Fretboard';
 import { TheoryEngine } from './core/TheoryEngine';
